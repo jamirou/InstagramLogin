6582db8c9473299f60a37da386d7801365377c8c
Jamiro <jamirocristian15@gmail.com>
2023-11-02 17:44:19 -0300

    Adding body pt1

--- app/src/main/java/com/jamirodev/instagramlogin/LoginScreen.kt
+++ app/src/main/java/com/jamirodev/instagramlogin/LoginScreen.kt
@@ -1,18 +1,34 @@
 package com.jamirodev.instagramlogin
 
 import android.app.Activity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen() {
@@ -22,7 +38,56 @@ fun LoginScreen() {
             .padding(8.dp)
     ) {
         Header(Modifier.align(Alignment.TopEnd))
+        Body(Modifier.align(Alignment.Center))
     }
+}
+
+@Composable
+fun Body(modifier: Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    Column(modifier = modifier) {
+        InstagramLogo()
+        Spacer(16.dp)
+        Email(email) { email = it }
+        Spacer(4.dp)
+        Password(password) { email = it }
+        Spacer(8.dp)
+        ForgotPassword(Modifier.align(Alignment.End))
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot password?",
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        color = Color(0xFF7EBBE5),
+        modifier = modifier
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Password(password: String, onTextChanged: (String) -> Unit) {
+    TextField(value = password, onValueChange = { onTextChanged(it) })
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Email(email: String, onTextChanged: (String) -> Unit) {
+    TextField(value = email, onValueChange = { onTextChanged(it) })
+}
+
+@Composable
+fun Spacer(space: Dp) {
+    Spacer(modifier = Modifier.size(space))
+}
+
+@Composable
+fun InstagramLogo() {
+    Image(painter = painterResource(id = R.drawable.insta), contentDescription = "Instagram logo")
 }
 
 @Composable
