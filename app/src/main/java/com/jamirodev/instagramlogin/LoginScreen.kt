9b0ecb0c91cf1d37237b2910e6c813230b35f635
Jamiro <jamirocristian15@gmail.com>
2023-11-02 20:23:58 -0300

    container and content colors added

--- app/src/main/java/com/jamirodev/instagramlogin/LoginScreen.kt
+++ app/src/main/java/com/jamirodev/instagramlogin/LoginScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -170,7 +171,13 @@ fun LoginButton(loginEnabled: Boolean) {
     Button(
         onClick = { },
         enabled = loginEnabled,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF4EA8E9),
+            disabledContainerColor = Color(0xFF78C8F9),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        )
     ) {
         Text(text = "Log in")
     }
